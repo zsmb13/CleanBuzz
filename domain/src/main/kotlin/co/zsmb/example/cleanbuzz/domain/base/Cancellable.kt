d51fb9252a41e15c96de624a67fe84fbbadfbf29
zsmb13 <braunmarci@gmail.com>
2017-01-20 13:35:59 +0100

    Removed non-working cancel mechanism

--- domain/src/main/kotlin/co/zsmb/example/cleanbuzz/domain/base/Cancellable.kt
+++
@@ -1,7 +0,0 @@
-package co.zsmb.example.cleanbuzz.domain.base
-
-interface Cancellable {
-
-    fun cancel()
-
-}
--- domain/src/main/kotlin/co/zsmb/example/cleanbuzz/domain/base/UseCase.kt
+++ domain/src/main/kotlin/co/zsmb/example/cleanbuzz/domain/base/UseCase.kt
@@ -2,22 +2,13 @@ package co.zsmb.example.cleanbuzz.domain.base
 
 import rx.Observable
 import rx.Scheduler
-import rx.subscriptions.Subscriptions
 
-abstract class UseCase<T, in P>: Cancellable {
-
-    private var subscription = Subscriptions.empty()
+abstract class UseCase<T, in P> {
 
     protected abstract fun createObservable(params: P): Observable<T>
 
     fun execute(worker: Scheduler, params: P): Observable<T>
             = createObservable(params)
             .subscribeOn(worker)
-
-    override fun cancel() {
-        if (!subscription.isUnsubscribed) {
-            subscription.unsubscribe()
-        }
-    }
 
 }
