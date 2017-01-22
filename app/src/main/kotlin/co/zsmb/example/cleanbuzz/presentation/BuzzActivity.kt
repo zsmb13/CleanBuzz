7dcbd8e73cd91e1fc599f883986affb815b2ab67
zsmb13 <braunmarci@gmail.com>
2017-01-22 21:20:50 +0100

    Removed debug feature

--- app/src/main/kotlin/co/zsmb/example/cleanbuzz/presentation/BuzzActivity.kt
+++ app/src/main/kotlin/co/zsmb/example/cleanbuzz/presentation/BuzzActivity.kt
@@ -2,16 +2,12 @@ package co.zsmb.example.cleanbuzz.presentation
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import co.zsmb.example.cleanbuzz.BuildConfig
 import co.zsmb.example.cleanbuzz.R
 import co.zsmb.example.cleanbuzz.di.activity.BuzzActivityComponent
 import co.zsmb.example.cleanbuzz.di.activity.DaggerBuzzActivityComponent
 import co.zsmb.example.cleanbuzz.di.application.BuzzApplication
 import co.zsmb.example.cleanbuzz.presentation.base.BaseView
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.onClick
 import org.jetbrains.anko.textColor
 
@@ -38,24 +34,6 @@ class BuzzActivity : BaseView<BuzzPresenter, BuzzActivityComponent>(), BuzzView 
         btnBuzz.onClick {
             presenter.requestNumber(etNumber.text.toString())
         }
-    }
-
-    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
-        if (BuildConfig.DEBUG) {
-            menuInflater.inflate(R.menu.menu_main, menu)
-            return true
-        }
-        return super.onPrepareOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (BuildConfig.DEBUG) {
-            if (item.itemId == R.id.action_new_activity) {
-                startActivity(intentFor<BuzzActivity>())
-                return true
-            }
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun showResult(result: PresentableResult) {
